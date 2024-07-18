99b7789e35d451ddba1f5d9f06ca25f8cf652e42
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-07-18 16:22:35 +0530

    add env form

--- src/app/component/side-bar/side-bar.component.ts
+++ src/app/component/side-bar/side-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Directive, ElementRef, HostListener, Input } from '@angular/core';
 import { AppState } from 'src/app/store/node.state';
 import * as NodeActions from '../../store/node.actions';
 import { NodeType, Node } from 'src/app/models/node.model';
 import { Store } from '@ngrx/store';
+import { EnvVariable } from '@/app/models/env.model';
+
 
 @Component({
   selector: 'app-side-bar',
@@ -11,13 +13,49 @@ import { Store } from '@ngrx/store';
 })
 export class SideBarComponent {
   activeTab: string = 'canvas';
+  newEnvKey: string = '';
+  newEnvValue: string = '';
+  envVariables: EnvVariable[] = [];
+  NodeType = NodeType
 
-  NodeType = NodeType;
-
-  setActiveTab(tabName: string) {
-    this.activeTab = tabName;
+  constructor(private store: Store<{ appState: AppState }>) {
+    this.store.select(state => state.appState.envVariables).subscribe(envVariables => {
+      this.envVariables = envVariables;
+    });
   }
-  constructor(private store: Store<{ appState: AppState }>) { }
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  addEnvVariable(nextInput: HTMLElement) {
+    nextInput.blur();
+    const envVariable: EnvVariable = { key: this.newEnvKey, value: this.newEnvValue };
+    this.store.dispatch(NodeActions.addEnvVariable({ envVariable }));
+    this.newEnvKey = '';
+    this.newEnvValue = '';
+  }
+
+  capitalize() {
+    this.newEnvKey = this.newEnvKey.toUpperCase();
+  }
+
+  deleteEnvVariable(key: string) {
+    this.store.dispatch(NodeActions.deleteEnvVariable({ key }));
+  }
+
+  editEnvVariable(key: string) {
+    const envVariable = this.envVariables.find(env => env.key === key);
+    if (envVariable) {
+      this.newEnvKey = envVariable.key;
+      this.newEnvValue = envVariable.value;
+      this.deleteEnvVariable(key);
+    }
+  }
+
+  moveToNextInput(nextInput: HTMLInputElement) {
+    nextInput.focus();
+  }
 
   addNode(nodeType: NodeType): void {
     const newNode: Node = {
