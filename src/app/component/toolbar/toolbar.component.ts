9a69a33d23e4bc62e67b59d8863b092675ffa847
prakhar-5447 <sahuprakhar022003@gmail.com>
2024-07-21 15:56:16 +0530

    add connection method

--- src/app/component/toolbar/toolbar.component.ts
+++ src/app/component/toolbar/toolbar.component.ts
@@ -1,4 +1,8 @@
+import { AppState } from '@/app/store/node.state';
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import * as NodeSelectors from '../../store/node.selectors';
+import { Connection } from '@/app/models/connection.model';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.sass']
 })
 export class ToolbarComponent {
-
+  selectedConnection: Connection | null = null;
+  constructor(private store: Store<{ appState: AppState }>) {
+    this.store.pipe(select(NodeSelectors.selectSelectedConnection)).subscribe(conn => {
+      this.selectedConnection = conn;
+    });
+  }
 }
